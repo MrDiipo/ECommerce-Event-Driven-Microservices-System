5774a7874e7ee2cb0e1f27f11934abb7cafb55e8
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-19 12:49:49 +0100

    Register ProductsService with Eureka Server

--- Products-Service/src/main/java/com/mrdiipo/productsservice/ProductsServiceApplication.java
+++ Products-Service/src/main/java/com/mrdiipo/productsservice/ProductsServiceApplication.java
@@ -2,7 +2,9 @@ package com.mrdiipo.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProductsServiceApplication {
 
