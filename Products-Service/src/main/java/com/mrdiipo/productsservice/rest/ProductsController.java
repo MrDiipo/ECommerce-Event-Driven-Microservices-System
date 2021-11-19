4ee5b003d355695c3fdd150b97e9e6179c7efa03
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-19 12:01:11 +0100

    Add Eureka Discovery Server Module

---
+++ Products-Service/src/main/java/com/mrdiipo/productsservice/rest/ProductsController.java
@@ -0,0 +1,9 @@
+package com.mrdiipo.productsservice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/products") // http://localhost:8080/api/v1/products
+public class ProductsController {
+}
