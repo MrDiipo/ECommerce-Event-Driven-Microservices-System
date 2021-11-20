cf42f5da411320bfcf8bb63c4b009f0001791e02
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-20 02:29:21 +0100

    Implement CreateProductCommand

--- Products-Service/src/main/java/com/mrdiipo/productsservice/rest/PostProductsController.java
+++ Products-Service/src/main/java/com/mrdiipo/productsservice/rest/PostProductsController.java
@@ -1,18 +1,50 @@
 package com.mrdiipo.productsservice.rest;
 
+import com.mrdiipo.productsservice.commands.CreateProductCommand;
 import com.mrdiipo.productsservice.rest.restModels.CreateProductRestModel;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping(path = "/api/v1/products") // http://localhost:8080/api/v1/products
 public class PostProductsController {
 
+    private final Environment environment;
+    private final CommandGateway commandGateway;
+
+    @Autowired
+    public PostProductsController(Environment environment,
+                                  CommandGateway commandGateway) {
+        this.environment = environment;
+        this.commandGateway = commandGateway;
+    }
+
+
     @PostMapping
     public String createProduct(@RequestBody CreateProductRestModel createProductRestModel){
-        return null;
+
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .price(createProductRestModel.getPrice())
+                .quantity(createProductRestModel.getQuantity())
+                .title(createProductRestModel.getTitle())
+                .productId(UUID.randomUUID().toString())
+                .build();
+
+        String returnValue;
+
+        try{
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e){
+            returnValue = e.getLocalizedMessage();
+        }
+        return returnValue;
     }
 
 }
