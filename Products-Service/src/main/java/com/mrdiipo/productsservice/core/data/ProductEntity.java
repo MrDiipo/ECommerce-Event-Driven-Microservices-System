ca73ea466cdc7208f07ce85f890ccf2ec5a0e536
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-11-22 17:57:58 +0100

    Create ProductRepository interface

--- Products-Service/src/main/java/com/mrdiipo/productsservice/core/data/ProductEntity.java
+++ Products-Service/src/main/java/com/mrdiipo/productsservice/core/data/ProductEntity.java
@@ -1,11 +1,29 @@
 package com.mrdiipo.productsservice.core.data;
 
+import lombok.Data;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
 
+@Data
+@Entity
+@Table(name = "products")
 public class ProductEntity implements Serializable {
 
     @Serial
     private static final long serialVersionUID = -2671294608197172601L;
+
+    @Id
+    @Column(unique = true)
+    private String productId;
+
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
 }
